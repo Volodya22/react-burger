7666a28f4b027aa46694b8fb0322e57bef89f626
VladimirCheboksarov <cheboxarov22@gmail.com>
2024-10-23 01:53:51 +0700

    Фикс скроллбара

--- src/components/burger-ingredients/burger-ingredient-tab/burger-ingredient-tab.tsx
+++ src/components/burger-ingredients/burger-ingredient-tab/burger-ingredient-tab.tsx
@@ -4,7 +4,7 @@ import styles from './burger-ingredient-tab.module.scss'
 export const BurgerIngredientTab = (props: any) => {
   return (
     <>
-      <h3 style={{ marginTop: 40, marginBottom: 0 }} className="text text_type_main-medium">{props.name}</h3>
+      <h3 style={{ marginTop: 0, marginBottom: 0 }} className="text text_type_main-medium">{props.name}</h3>
       <div className={styles.itemsContainer}>
         { props.items && props.items.map((x: any) => (
           <BurgerIngredientItem image={x.image} price={x.price} name={x.name} />
