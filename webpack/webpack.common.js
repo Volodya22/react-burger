2de8fb26b5fa6ba2364df7ffe1efb493a2ccbf40
VladimirCheboksarov <cheboxarov22@gmail.com>
2024-12-27 17:11:24 +0700

    hash update

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -10,9 +10,7 @@ module.exports = {
 	entry: path.resolve(__dirname, '..', './src/index.tsx'), //точка входа в наше приложение содержит абсолютный путь к index.ts
 	output: {
 		path: path.resolve(__dirname, '..', './build'), //путь куда будет собираться наш проект
-		filename: production
-			? 'static/scripts/[name].[contenthash].js'
-			: 'static/scripts/[name].js', // имя нашего бандла
+		filename: 'static/scripts/[name].js', // имя нашего бандла
 		publicPath: '/',
 	},
 	//Нужно помочь вебпаку научится работать с jsx и tsx файлами для этого используют ts loader
@@ -83,9 +81,7 @@ module.exports = {
 		}),
 		new CleanWebpackPlugin(),
 		new MiniCssExtractPlugin({
-			filename: production
-				? 'static/styles/[name].[contenthash].css'
-				: 'static/styles/[name].css',
+			filename: 'static/styles/[name].css',
 		}),
 		new webpack.EnvironmentPlugin({
 			NODE_ENV: 'development', // значение по умолчанию 'development' если переменная process.env.NODE_ENV не передана
